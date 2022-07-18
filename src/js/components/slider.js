70076a21cb1afd210b2c0660194cc8a8d8752c74
SergiyLuchko <LuchkoSergiy85@gmail.com>
2022-07-18 23:18:56 +0300

    add diet slider

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -20,3 +20,41 @@ const swiper = new Swiper('.about-swiper', {
 	slidesPerView: 2,
 
 });
+const swiper1 = new Swiper('.diet-swiper', {
+	pagination: {
+		el: '.diet-swiper__pagination',
+		clickable: true,
+	},
+  slidesPerView: 4,
+	loopedSlides: true,
+  breakpoints: {
+    // when window width is >= 320px
+   320: {
+      slidesPerView: 1.5,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    },
+    1100: {
+      slidesPerView: 4,
+      spaceBetween: 25
+    }
+  },
+
+	loopedSlides: true,
+
+	// autoHeight: true,
+	// spaceBetween: 24,
+
+
+
+});
+
